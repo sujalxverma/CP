4e7abb24dc2f8315b93dedef2bf7b85284fa4e87
sujal <sujalxv@gmail.com>
2025-06-24 09:56:03 +0530

    initial commit

---
+++ Algorithms/Boilerplate.cpp
@@ -0,0 +1,54 @@
+#include "bits/stdc++.h"
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<long long> vl;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rep2(i, a, b) for (long long i = a; i < b; i++)
+typedef unordered_map<int, int> umap;
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define even(a) (((a) % 2) == 0 ? 1 : 0)
+#define rev(v) reverse(v.begin(), v.end())
+#define prime(n) ([](int num) {if (num <= 1) return false;if (num == 2) return true;if (num % 2 == 0) return false;for (int i = 3; i <= std::sqrt(num); i += 2) {if (num % i == 0) return false;}return true; })(n)
+#define gcd(a, b) ([](int x, int y) {while (y != 0) { int temp = y;y = x % y; x = temp;}return x; })(a, b)
+#define lcm(a, b) (a * b / gcd(a, b))
+#define sorting(v) sort(v.begin(), v.end())
+#define line cout << endl
+#define contains(vec, x) (std::find((vec).begin(), (vec).end(), (x)) != (vec).end())
+#define containsBS(vec, x) (std::binary_search((vec).begin(), (vec).end(), (x)))
+#define ROTATE_VEC(v, k)                                 \
+    do                                                   \
+    {                                                    \
+        int __n = (int)(v).size();                       \
+        if (__n > 0)                                     \
+        {                                                \
+            int __k = (k) % __n;                         \
+            if (__k != 0)                                \
+            {                                            \
+                reverse((v).begin(), (v).end());         \
+                reverse((v).begin(), (v).begin() + __k); \
+                reverse((v).begin() + __k, (v).end());   \
+            }                                            \
+        }                                                \
+    } while (0)
+
+const int MOD = 100000;
+
+void solve()
+{
+
+}
+int main()
+{
+
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+}
