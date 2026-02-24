0d9be38c87d254ffb8dfe20b007efff9c426cec5
sujal <sujalxv@gmail.com>
2026-02-24 16:52:37 +0530

    use of #ifdef

---
+++ OffByOne/gunther.cpp
@@ -0,0 +1,25 @@
+/*
+Use of #ifdef and #endif.
+
+How it Works ?
+- We define macro called DEBUG.
+- Now #ifdef, will execute the code written in this scope, only if this
+  macro exist, otherwisw it will ignore it.
+*/
+
+#include <iostream>
+using namespace std;
+
+#define DEBUG
+
+int main()
+{
+
+#ifdef DEBUG
+    cout << "Debug is ON\n";
+#endif
+
+    cout << "Hello Sujal\n";
+
+    return 0;
+}
