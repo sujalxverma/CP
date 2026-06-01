95f3d0b371cf6653e73e0e516b0a1598e49ae7d4
sujal <sujalxv@gmail.com>
2026-06-01 16:04:17 +0530

    template-update

--- Algorithms/template.cpp
+++ Algorithms/template.cpp
@@ -7,8 +7,6 @@ using namespace std;
 
 using ll = long long;
 using ld = long double;
-using vi = vector<int>;
-using vl = vector<long long>;
 
 std::mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
 
@@ -22,18 +20,19 @@ static inline void fastio()
   ios::sync_with_stdio(false);
   cin.tie(nullptr);
 }
-
 // Bit helpers (careful with types)
+// #define BITHELPER
+#ifdef BITHELPER
 static inline int setbits(unsigned int x) { return __builtin_popcount(x); }
-static inline int setbitsll(unsigned long long x)
-{
-  return __builtin_popcountll(x);
-}
+static inline int setbitsll(unsigned long long x) { return __builtin_popcountll(x); }
 static inline int leadingzero(unsigned int x) { return __builtin_clz(x); }
 static inline int trailingzero(unsigned int x) { return __builtin_ctz(x); }
 static inline int parity(unsigned int x) { return __builtin_parity(x); }
+#endif
 
 // Math utils
+// #define MATHS
+#ifdef MATHS
 int gcd(int a, int b)
 {
   if (b == 0)
@@ -41,53 +40,128 @@ int gcd(int a, int b)
   else
     return gcd(b, a % b);
 }
-static inline bool is_prime_ll(long long n)
-{
-  if (n < 2)
-    return false;
-  if ((n & 1LL) == 0)
-    return n == 2;
-  for (long long d = 3; d * d <= n; d += 2)
-  {
-    if (n % d == 0)
-      return false;
-  }
-  return true;
-}
 
-// Mod ops
-static inline int norm_mod(long long x)
+static inline long long mod_pow(long long base, long long exp, long long mod)
 {
-  x %= MOD;
-  if (x < 0)
-    x += MOD;
-  return (int)x;
-}
-static inline int mod_add(int a, int b)
-{
-  return a + b >= MOD ? a + b - MOD : a + b;
-}
-static inline int mod_sub(int a, int b)
-{
-  return a - b < 0 ? a - b + MOD : a - b;
-}
-static inline int mod_mul(int a, int b) { return int(1LL * a * b % MOD); }
-static inline int mod_pow(int a, long long e)
-{
-  int r = 1;
-  while (e)
-  {
-    if (e & 1LL)
-      r = mod_mul(r, a);
-    a = mod_mul(a, a);
-    e >>= 1LL;
-  }
-  return r;
-}
-static inline int mod_inv(int a) { return mod_pow(a, MOD - 2); } // MOD prime
+  long long res = 1;
+  base %= mod;
 
+  while (exp > 0)
+  {
+    if (exp & 1)
+      res = (res * base) % mod;
+
+    base = (base * base) % mod;
+    exp >>= 1;
+  }
+
+  return res;
+}
+#endif
+
+// #define SEGMENTTREE
+#ifdef SEGMENTTREE
+struct Node
+{
+  long long val; // change type as needed
+
+  Node(long long v = 0)
+  {
+    val = v;
+  }
+};
+
+struct SegTree
+{
+  int size;
+  int real_size;
+  vector<Node> tree;
+  Node NEUTRAL = Node(0); // neutral element for merge
+
+  // merge two nodes
+  // can be modified.
+  Node merge(const Node &a, const Node &b)
+  {
+    return Node(a.val + b.val); // example: sum segment tree
+  }
+
+  // initialize tree
+  void init(int n)
+  {
+    real_size = n;
+    size = 1;
+    while (size < n)
+      size <<= 1;
+    tree.assign(2 * size, NEUTRAL);
+  }
+
+  // build from array
+  void build(vector<int> &arr, int node, int lx, int rx)
+  {
+    if (rx - lx == 1)
+    {
+      if (lx < (int)arr.size())
+        tree[node] = Node(arr[lx]);
+      return;
+    }
+
+    int mid = (lx + rx) / 2;
+    build(arr, 2 * node + 1, lx, mid);
+    build(arr, 2 * node + 2, mid, rx);
+    tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
+  }
+
+  void build(vector<int> &arr)
+  {
+    build(arr, 0, 0, size);
+  }
+
+  // point update: set position i to value v
+  void set(int i, int v, int node, int lx, int rx)
+  {
+    if (rx - lx == 1)
+    {
+      tree[node] = Node(v);
+      return;
+    }
+
+    int mid = (lx + rx) / 2;
+    if (i < mid)
+      set(i, v, 2 * node + 1, lx, mid);
+    else
+      set(i, v, 2 * node + 2, mid, rx);
+
+    tree[node] = merge(tree[2 * node + 1], tree[2 * node + 2]);
+  }
+
+  void set(int i, int v)
+  {
+    set(i, v, 0, 0, size);
+  }
+
+  // range query [l, r), to include r => [l,r+1)
+  // int x -> current node of the tree.
+  Node query(int l, int r, int node, int lx, int rx)
+  {
+    if (rx <= l || r <= lx)
+      return NEUTRAL;
+    if (l <= lx && rx <= r)
+      return tree[node];
+
+    int mid = (lx + rx) / 2;
+    Node left = query(l, r, 2 * node + 1, lx, mid);
+    Node right = query(l, r, 2 * node + 2, mid, rx);
+    return merge(left, right);
+  }
+
+  Node query(int l, int r)
+  {
+    return query(l, r, 0, 0, size);
+  }
+};
+#endif
 void solve()
-{ // TODO: implement per test case solution.
+{
 }
 
 int main()
